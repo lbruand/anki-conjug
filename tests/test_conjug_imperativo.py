e78c88a644ed4f67aa2177d85f31b834eebfcc62
Lucas Bruand <l.bruand.pro@gmail.com>
2025-06-07 09:43:52 +0200

    Added speech generation

--- tests/test_conjug_imperativo.py
+++ tests/test_conjug_imperativo.py
@@ -1,15 +1,22 @@
+import io
 import random
 import re
 import unittest
 import json
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Set
+from wave import Wave_read
 
 import genanki
+import mp3
+import numpy as np
 import ollama
+import scipy
 import spacy
 import tqdm
 from pydantic import BaseModel
+from transformers import AutoProcessor, BarkModel
 from verbecc import Conjugator
 
 
@@ -27,11 +34,17 @@ pronouns = ["yo", "tú", "él", "nosotros", "vosotros", "ellos"]
 pronouns_imperativo = pronouns[1:]
 
 
-@dataclass(frozen=True, unsafe_hash=True)
+@dataclass
 class Note:
     question: str
     response: str
+    original_sentence: str
+    reading: Path = None
+    def __hash__(self):
+        return hash(self.original_sentence)
 
+    def __eq__(self, other):
+        return self.original_sentence.__eq__(other.original_sentence)
 
 class ResultSafe(BaseModel):
   safe: bool
@@ -41,13 +54,13 @@ class ResultSafe(BaseModel):
 
 def generate(cg = Conjugator(lang='es'),
              rand = random.Random(3),
-             nlp = spacy.load("es_core_news_sm")):
+             nlp = spacy.load("es_core_news_sm")) -> Note:
 
     mood = "imperativo"
     verb_infinitive = rand.choice(verbs)
     conjug = cg.conjugate(verb_infinitive)
     #verb_infinitive = conjug['verb']['inifinitive']
-    conjug_imperativo = conjug["moods"]["imperativo"]
+    conjug_imperativo = conjug["moods"][mood]
     tipo = rand.choice(list(conjug_imperativo.values()))
     alpha, beta = 0.3, 0.1
 
@@ -81,8 +94,9 @@ def generate(cg = Conjugator(lang='es'),
     result_content_highlighted = re.sub(f"({conjug_verb})",
                                         replace_value_highlight,
                                         result_content, flags=re.IGNORECASE)
-    return dict(question=result_content_ablated,
-                response=result_content_highlighted)
+    return Note(question=result_content_ablated,
+                response=result_content_highlighted,
+                original_sentence=result_content)
 
 
 def guardrail(rand, result_content):
@@ -137,7 +151,69 @@ def ispresent_sublist_in_list(sublist, l):
             return ispresent_sublist_in_list(sublist, l[1:])
 
 
+def generate_audio(sentence: str,
+                   output_file: Path,
+                   voice_preset = "v2/es_speaker_8",
+                   processor = AutoProcessor.from_pretrained("suno/bark"),
+                   model = BarkModel.from_pretrained("suno/bark"),
+                   ) -> np.array:
+    inputs = processor(f"- {sentence}", voice_preset=voice_preset)
+
+    audio_array = model.generate(**inputs)
+    audio_array = audio_array.cpu().numpy().squeeze()
+    sample_rate = model.generation_config.sample_rate
+
+    max_val = np.max(np.abs(audio_array))
+
+    if max_val > 0:
+        data_normalized = audio_array / max_val
+    else:
+        data_normalized = audio_array
+
+    data_int16 = np.int16(data_normalized * 32767)
+
+    with io.BytesIO() as fout:
+        scipy.io.wavfile.write(fout, rate=sample_rate, data=data_int16)
+        all_bytes = fout.getvalue()
+
+    with io.BytesIO(all_bytes) as fin:
+        wav_file = Wave_read(fin)
+
+        sample_size = wav_file.getsampwidth()
+        sample_rate = wav_file.getframerate()
+        nchannels = wav_file.getnchannels()
+
+        if sample_size != 2:
+            raise ValueError("Only PCM 16-bit sample size is supported (input audio: %s)" % sample_size)
+
+        with open(output_file, 'wb') as fout:
+            encoder = mp3.Encoder(fout)
+            encoder.set_bit_rate(64)
+            encoder.set_sample_rate(sample_rate)
+            encoder.set_channels(nchannels)
+            encoder.set_quality(5)
+            encoder.set_mode(mp3.MODE_SINGLE_CHANNEL)
+            while True:
+                pcm_data = wav_file.readframes(8000)
+                if pcm_data:
+                    encoder.write(pcm_data)
+                else:
+                    encoder.flush()
+                    break
+
+    return audio_array
+
 class MyTestCase(unittest.TestCase):
+
+    def test_generate_speech(self):
+        print("hello")
+        output_dir = Path('output')
+        output_dir.mkdir(parents=True, exist_ok=True)
+        output_file = output_dir / 'vivamos.mp3'
+        audio_array = generate_audio("Vivamos al aire libre todos los fines de semana.",
+                                     output_file)
+
+
     def test_find_sublist_in_list(self):
         self.assertTrue(ispresent_sublist_in_list(list(range(0, 3)), list(range(0, 8))))
         self.assertTrue(ispresent_sublist_in_list(list(range(3, 5)), list(range(0, 8))))
@@ -155,11 +231,28 @@ class MyTestCase(unittest.TestCase):
     def test_wholedeck(self):
         rand = random.Random(3)
 
-        work = [generate(cg=Conjugator(lang='es'), rand=rand, nlp=spacy.load("es_core_news_sm")) for i in range(50)]
-        notes = set([item for item in work if item])
+        cg = Conjugator(lang='es')
+        nlp = spacy.load("es_core_news_sm")
+        work = [generate(cg=cg, rand=rand, nlp=nlp) for i in range(70)]
+        notes: Set[Note] = set([item for item in work if item])
 
         output_dir = Path('output')
         output_dir.mkdir(parents=True, exist_ok=True)
+        media_files = ['../img/_background_tiles2.jpg']
+
+        voice_preset = "v2/es_speaker_8"
+        processor = AutoProcessor.from_pretrained("suno/bark")
+        model = BarkModel.from_pretrained("suno/bark")
+        for ix, card in tqdm.tqdm(enumerate(notes)):
+            output_file = output_dir / f"card_{ix}.mp3"
+            generate_audio(card.original_sentence,
+                           output_file,
+                           voice_preset,
+                           processor,
+                           model)
+            card.reading = f"[sound:{output_file.name}]"
+            media_files.append(output_file)
+
         output_file = output_dir / 'imperative.apkg'
 
         my_model = genanki.Model(
@@ -168,12 +261,13 @@ class MyTestCase(unittest.TestCase):
             fields=[
                 {'name': 'Question'},
                 {'name': 'Answer'},
+                {'name': 'Reading'},
             ],
             templates=[
                 {
                     'name': 'Card 1',
                     'qfmt': '<div class="box"><div class="box-inside">{{Question}}</div></div>',
-                    'afmt': '<div class="box"><div class="box-inside">{{Answer}}</div></div>',
+                    'afmt': '<div class="box"><div class="box-inside">{{Answer}}<br/>{{Reading}}</div></div>',
                 },
             ],
             css="""
@@ -211,17 +305,19 @@ class MyTestCase(unittest.TestCase):
             """
         )
         my_deck = genanki.Deck(
-            205940084710,
+            205940088710,
             'Imperativo')
         for note in tqdm.tqdm(notes):
             my_note = genanki.Note(
                 model=my_model,
                 fields=[note.question,
-                        note.response])
+                        note.response,
+                        note.reading])
             my_deck.add_note(my_note)
 
         my_package = genanki.Package(my_deck)
-        my_package.media_files = ['../img/_background_tiles2.jpg']
+
+        my_package.media_files = media_files
         my_package.write_to_file(output_file)
         self.assertEqual(True, False)  # add assertion here
 
